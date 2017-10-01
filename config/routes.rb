844b7320a7f04619ef01e893cfd79e775fe1ca4d
Tyler <tbuchhei@alumni.nd.edu>
2017-10-01 13:31:37 -0500

    Add basic controllers and functional login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+
+  resources :users, only: [:show]
+
+  root to: "welcome#home"
+
 end
--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -2,11 +2,17 @@ module LoginHelper
 
   def visit_signin
     visit "/"
-    click_link "Sign In" 
+    click_link "Sign In"
   end
 
   def user_login
-    fill_in("user[name]", with: "tylerB")
+    User.create(
+      username: "tylerB",
+      email: "tyler@gmail.com",
+      password: "123456",
+      password_confirmation: "123456"
+    )
+    fill_in("user[username]", with: "tylerB")
     fill_in("user[password]", with: "123456")
     click_button("Sign In")
   end

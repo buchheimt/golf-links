cb972c542fa17d520d1f1b59c781e48334669bd8
Tyler <tbuchhei@alumni.nd.edu>
2017-10-04 13:03:50 -0500

    Add authorizations to User#show pages

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -77,8 +77,8 @@ describe "User Features", type: :features do
 
     before :each do
       User.create(
-        username: "tylerB",
-        email: "tyler@gmail.com",
+        username: "johnS",
+        email: "john@gmail.com",
         password: "123456",
         password_confirmation: "123456"
       )
@@ -88,36 +88,77 @@ describe "User Features", type: :features do
       User.first
     }
 
-    it "displays all of user's tee times" do
-      tee_time1 = TeeTime.new(time: Time.now)
-      tee_time1.build_course(name: "Augusta National GC")
-      tee_time1.save
-      user_tee_time1 = UserTeeTime.create(tee_time_id: tee_time1.id, user_id: user.id)
-      tee_time2 = TeeTime.new(time: Time.now)
-      tee_time2.build_course(name: "Pebble Beach Golf Links")
-      tee_time2.save
-      user_tee_time2 = UserTeeTime.create(tee_time_id: tee_time2.id, user_id: user.id)
-      visit user_path(user)
-      visit_signin
-      user_login
-      expect(page).to have_content("#{tee_time1.course.name} - #{tee_time1.time}")
-      expect(page).to have_content("#{tee_time2.course.name} - #{tee_time2.time}")
+    context "when correct user" do
+      it "links to User/TeeTime#new" do
+        visit_signin
+        user_login
+        visit user_path(current_user)
+        click_link("Create New Tee Time")
+        expect(current_path).to eq(new_user_tee_time_path(current_user))
+      end
+
+      it "links to User#edit" do
+        visit_signin
+        user_login
+        visit user_path(current_user)
+        click_link("Edit Profile")
+        expect(current_path).to eq(edit_user_path(current_user))
+      end
+
+      it "displays all of user's tee times" do
+        visit_signin
+        user_login
+        tee_time1 = TeeTime.new(time: Time.now)
+        tee_time1.build_course(name: "Augusta National GC")
+        tee_time1.save
+        user_tee_time1 = UserTeeTime.create(tee_time_id: tee_time1.id, user_id: current_user.id)
+        tee_time2 = TeeTime.new(time: Time.now)
+        tee_time2.build_course(name: "Pebble Beach Golf Links")
+        tee_time2.save
+        user_tee_time2 = UserTeeTime.create(tee_time_id: tee_time2.id, user_id: current_user.id)
+        visit user_path(current_user)
+        expect(page).to have_content("#{tee_time1.course.name} - #{tee_time1.time}")
+        expect(page).to have_content("#{tee_time2.course.name} - #{tee_time2.time}")
+      end
     end
 
-    it "links to User/TeeTime#new" do
-      visit_signin
-      user_login
-      visit user_path(user)
-      click_link("Create New Tee Time")
-      expect(current_path).to eq(new_user_tee_time_path(user))
+    context "when different user" do
+      it "doesn't display all of user's tee times" do
+        visit_signin
+        user_login
+        tee_time1 = TeeTime.new(time: Time.now)
+        tee_time1.build_course(name: "Augusta National GC")
+        tee_time1.save
+        user_tee_time1 = UserTeeTime.create(tee_time_id: tee_time1.id, user_id: user.id)
+        tee_time2 = TeeTime.new(time: Time.now)
+        tee_time2.build_course(name: "Pebble Beach Golf Links")
+        tee_time2.save
+        user_tee_time2 = UserTeeTime.create(tee_time_id: tee_time2.id, user_id: user.id)
+        visit user_path(user)
+        expect(page).to_not have_content("#{tee_time1.course.name} - #{tee_time1.time}")
+        expect(page).to_not have_content("#{tee_time2.course.name} - #{tee_time2.time}")
+      end
+
+      it "doesn't link to User/TeeTime#new" do
+        visit_signin
+        user_login
+        visit user_path(user)
+        expect(page).to_not have_content("Create New Tee Time")
+      end
+
+      it "doesn't link to User#edit" do
+        visit_signin
+        user_login
+        visit user_path(user)
+        expect(page).to_not have_content("Edit Profile")
+      end
     end
 
-    it "links to User/TeeTime#edit if current user's profile" do
-      visit_signin
-      user_login
-      visit user_path(user)
-      click_link("Edit Profile")
-      expect(current_path).to eq(edit_user_path(user))
+    context "when logged out" do
+      it "redirects to welcome page" do
+        visit user_path(user)
+        expect(current_path).to eq(root_path)
+      end
     end
   end
 
@@ -133,7 +174,7 @@ describe "User Features", type: :features do
       }
     end
 
-    context "correct user" do
+    context "when correct user" do
       it "allows user to properly edit info" do
         visit_signin
         user_login
@@ -166,7 +207,7 @@ describe "User Features", type: :features do
       end
     end
 
-    context "incorrect user" do
+    context "when different user" do
       it "redirects to welcome page" do
         user = User.create(user_attributes)
         visit_signin
@@ -176,7 +217,7 @@ describe "User Features", type: :features do
       end
     end
 
-    context "logged out" do
+    context "when logged out" do
       it "redirects to welcome page" do
         user = User.create(user_attributes)
         visit edit_user_path(user)

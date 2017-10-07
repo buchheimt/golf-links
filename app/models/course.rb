c3f654d9a0dff529983f44485b7f3b0a84c88afa
Tyler <tbuchhei@alumni.nd.edu>
2017-10-07 18:37:11 -0500

    Add feature to show active and total tee times for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,4 +6,8 @@ class Course < ApplicationRecord
   has_many :user_tee_times, through: :tee_times
   has_many :users, through: :user_tee_times
 
+  def active_tee_times
+    TeeTime.course_date_sort(self)
+  end
+
 end

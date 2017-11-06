110a6b12b80326c4a7d764fb424ff0549c5f1628
Tyler <tbuchhei@alumni.nd.edu>
2017-11-05 19:57:38 -0600

    Add unavailable class AJAX to Course#show updates

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -47,11 +47,12 @@ const loadTeeTimes = (teeTimes) => {
   const template = Handlebars.compile(templateSource);
   $("#teeTimeCards").empty();
   teeTimes.forEach(function(teeTime) {
-    console.log(teeTime.time)
-    console.log(teeTime.time >= Date.now())
     if (teeTime.active) {
       const $teeTimeDiv = $(template(teeTime));
       $("#teeTimeCards").append($teeTimeDiv);
+      if (!teeTime['available?']) {
+        $teeTimeDiv.children("div").addClass("tee-time-unjoinable");
+      }
     }
-  })
+  });
 }

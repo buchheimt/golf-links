8f516043406bc3982c7a749b4545f13dedc4da81
Tyler <tbuchhei@alumni.nd.edu>
2017-11-05 20:37:06 -0600

    Add functional image handling on course AJAX nav

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,4 +1,4 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :par, :length, :price, :description, :image
+  attributes :id, :name, :location, :par, :length, :price, :description, :get_image
   has_many :tee_times
 end

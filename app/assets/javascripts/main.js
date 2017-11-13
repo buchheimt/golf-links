29a1fa38377a9cd27c602c1ca41261a1838ad039
Tyler <tbuchhei@alumni.nd.edu>
2017-11-12 22:22:07 -0600

    Redesign responsive navbar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,4 +5,13 @@ $(function() {
       $.ajax({url: deleteRoute, method: "DELETE"});
     }
   });
+  $("#mobileNavBtn").click(function() {
+    const darkColor = "rgb(54, 79, 64)";
+    if ($("svg.octicon-grabber").css("fill") === darkColor ) {
+      $("svg.octicon-grabber").css({fill: "white"});
+    } else {
+      $("svg.octicon-grabber").css({fill: darkColor});
+    }
+    $(".mobileLinks").toggle();
+  });
 });
